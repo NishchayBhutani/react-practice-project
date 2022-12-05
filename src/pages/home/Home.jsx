40e2d9be47c3ad28f6771b8e611a5923e6843ab5
NishchayBhutani <bhutaninishchay9@gmail,com>
2022-12-05 16:18:59 +0530

    init commit

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./home.css";
+import Header from "../../components/header/Header";
+import Navbar from "../../components/navbar/Navbar";
+const Home = () => {
+  return (
+    <div className="home-container">
+      <Navbar />
+      <Header />
+    </div>
+  );
+};
+
+export default Home;
